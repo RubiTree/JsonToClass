63754ed675b8a581bef2a3eb4291312972b4dd59
xiongzhengyu <xiongzhengyu@bytedance.com>
2019-10-16 03:08:28 +0800

    failed test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import '../node_modules/reflect-metadata/Reflect.js'
+import 'reflect-metadata'
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { User } from './User';
+import { plainToClass } from "class-transformer";
+
 
 const App: React.FC = () => {
+
+  React.useEffect(() => {
+    const testJson = JSON.stringify(new User(2,'hello','world'))
+    const result = plainToClass(User, testJson)
+    console.log(`result.id:${result.id} result.firstName:${result.firstName} result.lastName:${result.lastName}`)
+  },[])
+
+
   return (
     <div className="App">
       <header className="App-header">
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import '../node_modules/reflect-metadata/Reflect.js'
+import 'reflect-metadata'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
