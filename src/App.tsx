ddeca7013bd70909fc40c2dec19726f38ff5f8a9
xiongzhengyu <xiongzhengyu@bytedance.com>
2019-10-16 13:38:21 +0800

    ignore

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ const App: React.FC = () => {
   React.useEffect(() => {
     const testJson = JSON.stringify(new User(2,'hello','world'))
     const result = plainToClass(User, testJson)
+
+    // ERROR RESULT!  result.id:undefined result.firstName:undefined result.lastName:undefined
     console.log(`result.id:${result.id} result.firstName:${result.firstName} result.lastName:${result.lastName}`)
   },[])
 
